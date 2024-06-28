395ce96a1ad9626af5788c53f4225aa00f66f9cb
ranmaru04 <ranmaru165@icloud.com>
2024-06-28 17:55:53 +0900

    Add files via upload

---
+++ execise.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int main(){
+    int i;
+    int count1=0,count2=0;
+    double num;
+    char* name;
+    srand(time(NULL));
+    printf("Who are you?\n");
+    printf(">");
+    scanf("%s",name);
+    printf("Hello, %s!\n",name);
+    printf("Tossing a coin\n");
+    for(i=1;i<4;i++){
+        //char t[6];
+        num=(double)rand()/RAND_MAX;
+        char* t;
+        if(num>0.5){
+             t = "Tails"; 
+             count1++;
+        }
+        else{
+             t = "Heads";
+             count2++;    
+        }
+        printf("Round %d:%s\n",i,t);
+    }
+    
+    printf("Heads: %d, Tails:%d\n",count2,count1);
+    return 0;
+}
